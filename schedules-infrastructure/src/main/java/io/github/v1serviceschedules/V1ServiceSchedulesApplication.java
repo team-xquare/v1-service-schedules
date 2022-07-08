5a84f2d76828bdb61f4089a3e19b123303b5164c
parksangwoo1617 <b01056721617@gmail.com>
2022-07-08 10:39:08 +0000

    Rename Files

--- schedules-infrastructure/src/main/java/io/github/v1serviceschedules/V1ServiceSchedulesApplication.java
+++ schedules-infrastructure/src/main/java/io/github/v1serviceschedules/V1ServiceSchedulesApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class V1ServiceApplication {
+public class V1ServiceSchedulesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(V1ServiceApplication.class, args);
+        SpringApplication.run(V1ServiceSchedulesApplication.class, args);
     }
 }
